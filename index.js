91e1a34aadd39f624c6ab95070fe1bff7e4aa76a
Douglas Flores <Dflores5df@icloud.com>
2023-03-10 09:21:15 -0500

    loops done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(array, eventName)
+{
+const array1 = ["Guadalupe", "Ollie", "Aki"];
+ for (let i = 0; i < array.length; i++)
+{
+array1[i] = (`Thank you, ${array1[i]}, for the wonderful ${eventName} gift!`);
+}
+    return array1; 
+}
+function countDown(num){
+    let count = num;
+    while (count >= 0){
+        console.log(count)
+        count--;
+    }
+}
